e97b69fdb2c272e846ff82f835e2e76658d83896
andresop <aotavop@gmail.com>
2023-05-12 11:38:25 -0500

    Clase 21 - Menu Carrito de compras - toggle

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const menuMobileIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const menuMobileIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const menuCarrito = document.querySelector(".product-detail");
 
-menuEmail.addEventListener('click',toggleDesktopMenu);
-menuMobileIcon.addEventListener('click',toggleMobileMenu);
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuMobileIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoMenu);
 
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+function toggleDesktopMenu() {
+  if (!menuCarrito.classList.contains("inactive")) {
+    toggleCarritoMenu();
+  }
+  desktopMenu.classList.toggle("inactive");
 }
 
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+function toggleMobileMenu() {
+  if (!menuCarrito.classList.contains("inactive")) {
+    toggleCarritoMenu();
+  }
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoMenu() {
+  if (!mobileMenu.classList.contains("inactive")) {
+    toggleMobileMenu();
+  }
+  if (!desktopMenu.classList.contains("inactive")) {
+    toggleDesktopMenu();
+  }
+  menuCarrito.classList.toggle("inactive");
 }
